343fe1c8fcd7b5ffb2ac2cf3411905869914546a
c-tech91 <computertechnologyycce@gmail.com>
2025-11-06 11:01:48 +0530

    Update PlantDetail.tsx

--- src/pages/PlantDetail.tsx
+++ src/pages/PlantDetail.tsx
@@ -1,4 +1,4 @@
-Tree Details:-  import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { 
   Leaf, 
